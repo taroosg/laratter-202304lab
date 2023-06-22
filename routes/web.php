8bbf64d4aafd5cce0d2c368ccd4f0f59c93ad684
taroosg <root@taroosg.dev>
2023-06-22 14:05:07 +0900

    init

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\FollowController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::middleware('auth')->group(function () {
+  // タイムライン
+  Route::get('/tweet/timeline', [TweetController::class, 'timeline'])->name('tweet.timeline');
+
+  // ユーザページ
+  Route::get('user/{user}', [FollowController::class, 'show'])->name('follow.show');
+
+  // follow関連
+  Route::post('user/{user}/follow', [FollowController::class, 'store'])->name('follow');
+  Route::post('user/{user}/unfollow', [FollowController::class, 'destroy'])->name('unfollow');
+
+  // favorite関連
+  Route::post('tweet/{tweet}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+  Route::post('tweet/{tweet}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+
+  Route::get('/tweet/mypage', [TweetController::class, 'mydata'])->name('tweet.mypage');
+  Route::resource('tweet', TweetController::class);
+});
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
